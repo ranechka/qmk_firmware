5e56ed42f07633cd9da3320c1bb058f716dfcd6f
ranechka <ranechka@gmail.com>
2020-09-07 20:56:56 -0500

    Adjusted mute to f23

--- keyboards/planck/keymaps/ranechka/keymap.c
+++ keyboards/planck/keymaps/ranechka/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|                                     
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |                                     
  * |------+------+------+------+------+------+------+------+------+------+------+------|                                     
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |NonUS\|  <>  | Home | End  |Keypd/|                                     
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |NonUS\|  <>  | Home | End  | F23  |                                     
  * |------+------+------+------+------+------+------+------+------+------+------+------|                                     
  * |   ,  |      |      |      |Lower |             |Raise | Next | Vol- | Vol+ | Play |                                     
  * `-----------------------------------------------------------------------------------'                                     
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD,    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,        KC_ASTR,              KC_LPRN,            KC_RPRN,          _______,
     KC_DELETE,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,        KC_PLUS,              KC_LCBR,            KC_RCBR,          KC_PIPE,
-    _______,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NONUS_HASH,  KC_NONUS_BSLASH,      KC_HOME,            KC_END,           KC_KP_SLASH,
+    _______,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NONUS_HASH,  KC_NONUS_BSLASH,      KC_HOME,            KC_END,           KC_F23,
     KC_COMMA,   _______,  _______,  _______,  _______,  _______,  KC_NO,    _______,        KC_MEDIA_NEXT_TRACK,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP,  KC_MEDIA_PLAY_PAUSE
 ),
 
